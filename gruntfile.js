0a496750a16d59c83796173043ea5d5253949705
Taylor Graham <tgraham.us@gmail.com>
2018-08-15 13:18:21 -0600

    gulp exercise 2

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) { /* this definese a node module */
     require('time-grunt')(grunt); /* shows time it takes for tasks */
     require('jit-grunt')(grunt, {
         useminPrepare: 'grunt-usemin'
-    }); /* ensures any other node modules will be loaded as implied */
+      }); /* ensures any other node modules will be loaded as implied */
 
     grunt.initConfig({/* this task converts scss to css */
         sass:{  /*<-- task to configure */
@@ -36,18 +36,22 @@ module.exports = function(grunt) { /* this definese a node module */
             }
         },
         /* the following are for grunt- install grunt-contrib-clean and grunt-contrib-copy */
-        copy: { 
-            html:{
-                files: [{
+        copy: {
+            html: {
+                files: [
+                {
+                    //for html
                     expand: true,
                     dot: true,
-                    cwd:'./',
-                    src:['*.html'],
+                    cwd: './',
+                    src: ['*.html'],
                     dest: 'dist'
-                }]            
+                }]                
             },
             fonts: {
-                files: [{
+                files: [
+                {
+                    //for font-awesome
                     expand: true,
                     dot: true,
                     cwd: 'node_modules/font-awesome',
@@ -56,29 +60,35 @@ module.exports = function(grunt) { /* this definese a node module */
                 }]
             }
         },
+
         clean: {
             build: {
-                src: ['dist/']
+                src: [ 'dist/']
             }
         },
-        /* use npm install --save-dev grunt-contrib-imagemin */
-        imagemin:{
-            dynamic:{
+         /* use npm install --save-dev grunt-contrib-imagemin */
+        imagemin: {
+            dynamic: {
                 files: [{
-                    expand: true,
-                    dot: true,
-                    cwd:'./',
-                    src:['img/*.{png,jpg,gif}'],
-                    dest: 'dist/'
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
                 }]
             }
-        },
+        }
+
+
+       
+      
 
         /* for the following: npm install --save-dave grunt-contrib-concat grunt-contrib-cssmin grunt-contrib-htmlmin grunt-contrib-uglify grunt-filerev grunt-usemin */
-        useminPrepare:{
-            foo:{
+        
+
+        useminPrepare: {
+            foo: {
                 dest: 'dist',
-                src:['contactus.html', 'aboutus.html', 'index.html']
+                src: ['contactus.html','aboutus.html','index.html']
             },
             options: {
                 flow: {
@@ -86,66 +96,85 @@ module.exports = function(grunt) { /* this definese a node module */
                         css: ['cssmin'],
                         js:['uglify']
                     },
-                    post:{
+                    post: {
                         css: [{
                             name: 'cssmin',
-                            createConfig: function (context,block){
-                                var generated = context.options.generated;
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
                                 generated.options = {
-                                    keepSpecialComments: 0,rebase: false
+                                    keepSpecialComments: 0, rebase: false
                                 };
-                            }
+                            }       
                         }]
                     }
                 }
             }
         },
+
+        // Concat
         concat: {
             options: {
                 separator: ';'
             },
-            dist: {},
-            uglify:{
-                dist: {}
-            },
-            cssmin:{
-                dist:{}
-            },
-            filerev: {
-                /* This adds a revision tag to the end of js and css files */
-                options:{
-                    encoding: 'utf8',
-                    algorithm: 'md5',
-                    length: 20
-                },
-                release: {
-                    files: [{
-                        src: [
-                            'dist/js/*.js',
-                            'dist/css/*.css'
-                        ]
-                    }]
-                }
-            }
+  
+            // dist configuration is provided by useminPrepare
+            dist: {}
         },
-        usemin:{
-            html: ['dist/contactus.html', 'dist/about.html','dist/index.html'],
+
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        // Filerev
+        filerev: {
             options: {
-                assetsDirs: ['dist', 'dist/css', 'dist/js']
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+  
+            release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
             }
         },
-        htmlmin: {
-            dist:{
-                options: {
+  
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css','dist/js']
+            }
+        },
+
+        htmlmin: {                                         // Task
+            dist: {                                        // Target
+                options: {                                 // Target options
                     collapseWhitespace: true
                 },
-                files: {
-                    'dist/index.html':'dist/index.html',
-                    'dist/contactus.html':'dist/contactus.html',
-                    'dist/aboutus.html':'dist/aboutus/html'
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'dist/index.html',  // 'destination': 'source'
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
                 }
             }
         }
+        
     });
 /* to execute the tasks above */
     grunt.registerTask('css'/* name of task - use this in coli*/,['npm insass']/* what the task involves */)
--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,82 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    broswerSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'), // to delete dist folder
+    imagemin=require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
-    /* configures gulp sass task: */
-gulp.task('sass', function(){
+
+ 
+gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('sass:watch', function() {
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+    });
+    
+    gulp.task('sass:watch', function () {
     gulp.watch('./css/*.scss', ['sass']);
-});
-
-gulp.task('browser-sync', function(){
-    var files = [
+    });
+    
+    gulp.task('browser-sync', function () {
+        var files = [
         './*.html',
         './css/*.css',
-        './js/*.js',
         './img/*.{png,jpg,gif}',
-    ];
-
-    browserSync.init(files,{
+        './js/*.js'
+        ];
+    
+        browserSync.init(files, {
         server: {
-            baseDir: './'
+            baseDir: "./"
         }
+        });
+    
     });
-});
+    
+    // Default task
+    gulp.task('default', ['browser-sync'], function() {
+        gulp.start('sass:watch');
+    });
+    
+    // to delete dist folder
+    gulp.task('clean', function() {
+        return del(['dist']);
+    });
+    
+    gulp.task('copyfonts', function() {
+       gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+       .pipe(gulp.dest('./dist/fonts'));
+    });
+   //no node needed to copy and paste, just pipe
 
-gulp.task('default', ['browser-sync'], function(){
-    gulp.start('sass:watch');
-})
+//require imagemin = require('gulp-imagemin'),
+    gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+    });
+
+    gulp.task('usemin', function() {
+        return gulp.src('./*.html')
+        .pipe(flatmap(function(stream, file){
+            return stream
+              .pipe(usemin({
+                  css: [ rev() ],
+                  html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+                  js: [ uglify(), rev() ],
+                  inlinejs: [ uglify() ],
+                  inlinecss: [ cleanCss(), 'concat' ]
+              }))
+          }))
+          .pipe(gulp.dest('dist/'));
+      });
+      
+      gulp.task('build',['clean'], function() {
+          gulp.start('copyfonts','imagemin','usemin');
+      });
